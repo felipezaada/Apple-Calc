442eb60a5fd57aac7d2dd41faff4cdb8d0423d1a
felipezaada <felipeeduardocarvalhopimenta@gmail.com>
2023-11-26 20:54:01 -0300

    save 6

--- src/main/java/com/mycompany/calcfelipe/Calculos.java
+++ src/main/java/com/mycompany/calcfelipe/Calculos.java
@@ -5,6 +5,7 @@
 package com.mycompany.calcfelipe;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 /**
  *
  * @author felip
@@ -13,6 +14,9 @@ public class Calculos {
     
     String txtNum1;
     String txtNum2;
+    String verifJ;
+    double resultadoF;
+    double parteDecimal;
     BigDecimal num1 = BigDecimal.ZERO;
     BigDecimal num2 = BigDecimal.ZERO;
     BigDecimal resultado = BigDecimal.ZERO;
@@ -22,31 +26,62 @@ public class Calculos {
         switch (escolha) {
             case '+':
                 resultado = num1.add(num2);
-                return resultado;
+                break;
             case '/':
-                resultado = num1.divide(num2);
-                return resultado;
+                resultadoF = num1.doubleValue() / num2.doubleValue();
+                parteDecimal =  resultadoF - Math.floor(resultadoF);
+                if(parteDecimal != 0){
+                    resultado = num1.divide(num2, 3, BigDecimal.ROUND_HALF_UP);
+                }else{
+                    resultado = num1.divide(num2, 0, BigDecimal.ROUND_HALF_UP);
+                }
+                break;
             case 'x':
                 resultado = num1.multiply(num2);
-                return resultado;
+                break;
             case '-':
                 resultado = num1.subtract(num2);
-                return resultado;
+                break;
             case '%':
                 resultado = num1.divide(BigDecimal.valueOf(100)).multiply(num2);
-                return resultado;
+                break;
             case ' ':
                 resultado = BigDecimal.ZERO;
-                return resultado;
-        }
+                break;
+        }     
+
         return resultado;
+        
     }
     
-    public void Inserir(javax.swing.JTextField jTextField1, char escolha, String click){
+    public void Inserir(javax.swing.JTextField jTextField1, char escolha, String click){   
         
-        if(resultado != BigDecimal.ZERO){
+        if(jTextField1.getText().equals(resultado.toString())){
+            click = "";
+        }
+        
+        if (resultado != BigDecimal.ZERO) {
             num1 = resultado;
-            jTextField1.setText("");
+        }
+        
+        if(escolha == '\0'){
+            txtNum1 = jTextField1.getText().trim().isEmpty() ? click : jTextField1.getText().trim() + click;
+            jTextField1.setText(txtNum1);
+            num1 = new BigDecimal(txtNum1);
+        }else if(escolha != '\0'){
+            txtNum2 = jTextField1.getText().trim().isEmpty() ? click : jTextField1.getText().trim() + click;
+            jTextField1.setText(txtNum2);
+            num2 = new BigDecimal(txtNum2);
+        }
+        
+    }
+
+    public void InserirPonto(javax.swing.JTextField jTextField1, char escolha, String click){
+        
+        verifJ = jTextField1.getText();
+        
+        if(verifJ.contains(String.valueOf('.'))){
+            click = "";
         }
         
         if(escolha == '\0'){
@@ -59,18 +94,5 @@ public class Calculos {
             num2 = new BigDecimal(txtNum2);
         }
     }
-
-    public void InserirPonto(javax.swing.JTextField jTextField1, char escolha, String click){
-     
-        if(escolha == '\0'){
-            txtNum1 = jTextField1.getText().trim().isEmpty() ? "." : jTextField1.getText().trim() + ".";
-            jTextField1.setText(txtNum1);
-            num1 = new BigDecimal(txtNum1);
-        }else{
-            txtNum2 = jTextField1.getText().trim().isEmpty() ? "." : jTextField1.getText().trim() + ".";
-            jTextField1.setText(txtNum2);
-            num2 = new BigDecimal(txtNum2);
-        }
-    }
-    
+   
 }
